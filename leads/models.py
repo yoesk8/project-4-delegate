58c49f78b294b0a6114e8f035719170404766385
yoesk8 <yoelgutierrez2021@yahoo.com>
2022-12-17 13:07:36 +0000

    Added needed fields to Task model

--- leads/models.py
+++ leads/models.py
@@ -3,7 +3,17 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Task(models.Model):
+
+    DEPARTMENT = (
+       ('Operations', 'Operations'),
+       ('FOH', 'FOH'),
+       ('Sales', 'Sales')
+    )
+    
     task_name = models.CharField(max_length=100)
     task_description = models.CharField(max_length=1000)
-    department = models.CharField(max_length=100)
-    task_priority = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(100)])
+    department = models.CharField(choices=DEPARTMENT, max_length=100)
+    task_priority = models.PositiveIntegerField(
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(100)])
+    task_picture = models.ImageField(blank=True, null=True)
+    needed_files = models.FileField(blank=True, null=True)
