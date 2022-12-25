12478c0c7db55885333f30f051c58a8fee34349b
yoesk8 <yoelgutierrez2021@yahoo.com>
2022-12-25 14:13:41 +0000

    Updated urls with django url name syntax to improve readability

--- leads/urls.py
+++ leads/urls.py
@@ -5,9 +5,9 @@ app_name = 'leads'
 
 
 urlpatterns = [
-    path('', task_list),
-    path('create/', task_create),
-    path('<int:pk>/', task_detail),
-    path('<int:pk>/update/', task_update),
-    path('<int:pk>/delete/', task_delete),
+    path('', task_list, name='task-list'),
+    path('create/', task_create, name='task-create'),
+    path('<int:pk>/', task_detail, name='task-detail'),
+    path('<int:pk>/update/', task_update, name='task-update'),
+    path('<int:pk>/delete/', task_delete, name='task-delete'),
 ]
