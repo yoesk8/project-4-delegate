c728803bedce6a16e50d7bbfc6778edc8da4608f
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-01-02 11:43:08 +0000

    Added password reset functionality

--- delegate/urls.py
+++ delegate/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from leads.views import landing_page, LandingPageView, SignupView
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (LoginView, LogoutView,
+                                       PasswordResetView, PasswordResetDoneView,
+                                       PasswordResetConfirmView,
+                                       PasswordResetCompleteView)
 
 
 app_name = 'leads'
@@ -14,6 +17,10 @@ urlpatterns = [
     path('leads/', include('leads.urls', namespace='leads')),
     path('staff/',  include('staff.urls', namespace="staff")),
     path('signup/', SignupView.as_view(), name='signup'),
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 
